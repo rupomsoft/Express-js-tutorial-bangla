e82fd8fb70199e45546e3643a8279cc3fab3b00d
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2020-11-05 23:25:05 +0600

    Express JS Routing
    Express JS Routing

--- PracticeApp1/index.js
+++ PracticeApp1/index.js
@@ -2,11 +2,24 @@ var express=require('express');
 
 app=express();
 
-
 app.get("/",function (req,res) {
-    res.send("Hello Express js");
+    res.send("Home Page ");
 });
 
+
+app.post("/about",function (req,res) {
+    res.send("About Page");
+});
+
+
+app.put("/contact",function (req,res) {
+    res.send("Contact Page");
+});
+
+
+app.delete("/terms",function (req,res) {
+    res.send("Terms Page");
+});
 
 app.listen(8000,function () {
     console.log("Server Run Success")
